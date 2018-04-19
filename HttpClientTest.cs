1120fc97310a2ccb6a1ea6e1acb357fad8b1c275
aserfend <liuchangm7j@vip.qq.com>
2018-04-19 19:11:23 +0800

    Demo

---
+++ HttpClientTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DotNet4.Utilities.UtilHttp;
+
+namespace DotNet4.Utilities.UtilHttp
+{
+	class HttpClientTest
+	{
+		private HttpClient http;
+		public void NormalHttp()
+		{
+			var task = new Thread(()=> {
+				Console.WriteLine("proxyBegin");
+				Thread.Sleep(5000);
+				http = new HttpClient();
+				http.GetHtml("http://www.baidu.com", callBack: (x) =>
+				{
+					Console.WriteLine(x.response.DataString(Encoding.UTF8).Length.ToString());
+					Thread.Sleep(5000);
+				});
+			});
+			task.Start();
+		}
+		
+	}
+}
